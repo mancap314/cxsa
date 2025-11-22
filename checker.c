19a756ee4dec8ab790b6d0f8a61226355399be27
mancap314 <egregius314@gmail.com>
2025-11-22 15:42:25 +0100

    omp parallelization added to checker

--- checker.c
+++ checker.c
@@ -14,24 +14,24 @@ int main(void) {
     uint16_t h = h_ratio * factor;
     
 
-    uint16_t x, y, offset;
     uint16_t max_offset = 60u;
-    char output_fp[256];
-    for (offset = 0; offset < max_offset; offset++) { 
+    #pragma omp parallel for schedule(dynamic)
+    for (uint16_t offset = 0; offset < max_offset; offset++) { 
+        char output_fp[256];
         /* Open output file corresponding to current offset */
         snprintf(output_fp, sizeof(output_fp), "checker-%04u.ppm", offset);
         FILE *f = fopen(output_fp, "wb");
         if (NULL == f) {
             fprintf(stderr, "[ERROR] Could not open %s because: %s\n", output_fp, strerror(errno));
-            return EXIT_FAILURE;
+            exit(EXIT_FAILURE);
         }
         /* Preamble output file */
         fprintf(f, "P6\n"
                 "%d %d\n"
                 "255\n", w, h);
         
-        for (y = 0; y < h; ++y) { /* iter rows */
-            for (x = 0; x < w; ++x) {
+        for (uint16_t y = 0; y < h; ++y) { /* iter rows */
+            for (uint16_t x = 0; x < w; ++x) {
                 if (((x + offset) / factor + (y + offset) / factor) % 2) {
                     fputc(0xFF, f); 
                     fputc(0x00, f); 
